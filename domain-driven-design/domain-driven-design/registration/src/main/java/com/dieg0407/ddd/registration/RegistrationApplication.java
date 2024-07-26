09f5701357a4d6f1033bf92a496c0d9c1a5e92f2
Dieg0407 <diego.pastor.0407@gmail.com>
2024-07-25 21:25:00 -0500

    feat: initiallezed domain driven design test project

---
+++ domain-driven-design/domain-driven-design/registration/src/main/java/com/dieg0407/ddd/registration/RegistrationApplication.java
@@ -0,0 +1,12 @@
+package com.dieg0407.ddd.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationApplication.class);
+    }
+}
