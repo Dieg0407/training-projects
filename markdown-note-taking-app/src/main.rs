8a1b4924668daafc7f6cc5e3c06c5d8666502d8f
Dieg0407 <diego.pastor.0407@gmail.com>
2025-08-04 23:11:24 -0500

    chore: basic reading

--- markdown-note-taking-app/src/main.rs
+++ markdown-note-taking-app/src/main.rs
@@ -1,5 +1,11 @@
+use std::fs::File;
+use std::io;
+
 mod parser;
 
-fn main() {
-    parser::parse_markdown();
+fn main() -> io::Result<()> {
+    let mut file = File::open("README.md")?;
+    let _ = parser::parse_markdown(&mut file);
+
+    return Ok(());
 }
--- markdown-note-taking-app/src/parser/mod.rs
+++ markdown-note-taking-app/src/parser/mod.rs
@@ -1,3 +1,21 @@
-pub fn parse_markdown() {
-    println!("Parse markdown called!");
+use std::io;
+use std::io::Read;
+
+pub fn parse_markdown<T: Read>(mut reader: T) -> io::Result<()> {
+    let mut buffer: [u8; 1024] = [0; 1024];
+
+    loop {
+        let bytes_read = reader.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+        let data = &buffer[0..bytes_read];
+
+        // TODO: not all u8 slices are valid utf8 strings
+        let data = String::from_utf8_lossy(data);
+
+        println!("Read data: {}", data);
+    }
+
+    Ok(())
 }
