b2d4a3ce08f2bc298d952cb5d709605b51750c7e
Dieg0407 <diego.pastor.0407@gmail.com>
2026-04-26 21:31:27 +0900

    chore: compose docker container

---
+++ pekko-debugging/src/main/java/com/dieg0407/test/pekko_debugging/PekkoDebuggingApplication.java
@@ -0,0 +1,12 @@
+package com.dieg0407.test.pekko_debugging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PekkoDebuggingApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PekkoDebuggingApplication.class, args);
+	}
+}
